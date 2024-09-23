7ff1f484c74bf9dc434e99969d6f7de30deb1127
Šukys Gustas <gustas.sukys@mif.stud.vu.lt>
2024-09-23 14:54:48 +0300

    Add GamesController and Game model

---
+++ Melon_Benchmark_API/Models/Game.cs
@@ -0,0 +1,11 @@
+namespace Melon_Benchmark_API.Models
+{
+    public class Game
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string Route { get; set; }
+
+    }
+}
