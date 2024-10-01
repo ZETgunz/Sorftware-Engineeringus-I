4aca2c4084f50eb457f6abffd5c0e79acdbc9001
Šukys Gustas <gustas.sukys@mif.stud.vu.lt>
2024-10-02 00:29:42 +0300

    Added stream inputs and outputs

---
+++ Melon_Benchmark_API/Stream/Stream.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Melon_Benchmark_API.Stream
+{
+    public class StreamHandler
+    {
+        private readonly string _filePath;
+
+        public StreamHandler(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void WriteJsonObject<T>(T obj)
+        {
+            string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            File.WriteAllText(_filePath, json);
+        }
+
+        public T ReadJsonObject<T>()
+        {
+            if (!File.Exists(_filePath))
+                throw new FileNotFoundException("The specified file does not exist.");
+
+            string json = File.ReadAllText(_filePath);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+        public void DeleteWrittenObject<T>()
+        {
+            if (!File.Exists(_filePath))
+                throw new FileNotFoundException("The specified file does not exist.");
+
+            var obj = ReadJsonObject<T>();
+            if (obj != null)
+            {
+                File.Delete(_filePath);
+            }
+            else
+            {
+                throw new InvalidOperationException("The specified object does not exist in the file.");
+            }
+        }
+    }
+}
