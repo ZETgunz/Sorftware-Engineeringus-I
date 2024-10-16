0100aa72684b3d716448c9ef7a53d963bc49d408
Arnas <arnas.vaitkus@gmail.com>
2024-10-16 15:49:12 +0300

    added leaderboard

--- frontend/src/components/Leaderboard/leaderboard.tsx
+++ frontend/src/components/Leaderboard/leaderboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './leaderboard.css'; // Make sure to create a corresponding CSS file for styling
 
 interface board {
@@ -8,8 +8,8 @@ interface board {
 
 export const Leaderboard: React.FC = () => {
 
-    //var [string, setString] = useState([""]);
-    var string = [""];
+    const [string, setString] = useState([""]);
+    var stringArr = [""];
 
     const fetchLeaderboard = async () => {
         try {
@@ -18,28 +18,31 @@ export const Leaderboard: React.FC = () => {
                 throw new Error(`Error: ${response.statusText}`);
             }
             const data: board[] = await response.json();
-            string.pop();
-            for(var i=0; i<data.length; i++){
-                string.push(`${data[i].username}: ${data[i].score}`);
+            string.length = 0;
+            for (var i = 0; i < data.length; i++) {
+                stringArr[i] = (`${data[i].username}: ${data[i].score}`);
                 //str += `${i.username}: ${i.score}`;
             }
-            console.log(string);
-            return string;
+            setString(stringArr);
+
+            console.log(stringArr);
+            return;
         } catch (error) {
             console.error('Error fetching leaderboard:', error);
         }
     }
 
-    fetchLeaderboard();
-
     return (
         <div className="leaderboard">
             <h1>Melon Leaderboards</h1>
-            {string.map((i: number) => (
+            <button onClick={() => fetchLeaderboard()}>Reveal</button>
+            {string.map((s, i) => (
                 <div key={i}>
-                    <label>{string}</label>
+                    <label>{s}</label>
                 </div>
             ))}
+
         </div >
     );
+
 }
