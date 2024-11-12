7d4dd93bcdb4a1a4a85cbf1d5544a6139afbe2a6
Gustas Šukys <gustas.sukys700@gmail.com>
2024-11-12 17:23:05 +0200

    Add migration for new game "Typing" and update descriptions

--- backend/Migrations/20241112152007_b.Designer.cs
+++ backend/Migrations/20241112152007_b.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backend.Data;
 
@@ -10,9 +11,11 @@ using backend.Data;
 namespace backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241112152007_b")]
+    partial class b
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Migrations/20241112152007_b.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class b : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Description",
+                value: "Slice as many Melons as possible.");
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Description",
+                value: "Remember the sequence and repeat it.");
+
+            migrationBuilder.InsertData(
+                table: "Games",
+                columns: new[] { "Id", "Description", "Name", "Route" },
+                values: new object[] { 3, "Type the text fast and correct.", "Typing", "/typing" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Description",
+                value: "Slice as many Melons as possible");
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Description",
+                value: "Remember the sequence and repeat it");
+        }
+    }
+}
